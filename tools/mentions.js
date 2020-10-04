a59028bf3dbfa6258f969c428f34590e902a4a91
NguyenXD06 <thinhnguyenhuuhung@gmail.com>
2020-10-04 18:13:54 +0200

    fixed mentions

--- tools/mentions.js
+++ tools/mentions.js
@@ -1,16 +1,16 @@
 module.exports = (value) => {
-    if (typeof (value) == "string") {
+    if(typeof(value) == "string"){
         if (value.startsWith("<") && value.endsWith(">")) {
-            value = value.slice(2, -1);
             if (value.includes("@")) {
+                value = value.slice(2, -1);
                 if (value.startsWith('!')) {
+                    value = value.slice(1);
+                }else if(value.startsWith('&')){
                     value = value.slice(1);
                 }
                 return value;
-            } else if (value.startsWith('&')) {
-                value = value.slice(1);
-                return value;
             } else if (value.includes("#")) {
+                value = value.slice(2, -1);
                 return value;
             } else {
                 return undefined;
@@ -24,20 +24,20 @@ module.exports = (value) => {
                 return undefined;
             }
         }
-    } else if (typeof (value) == "object") {
+    }else if(typeof(value) == "object"){
         let list = []
         value.forEach(v => {
             if (v.startsWith("<") && v.endsWith(">")) {
-                v = v.slice(2, -1);
                 if (v.includes("@")) {
+                    v = v.slice(2, -1);
                     if (v.startsWith('!')) {
+                        v = v.slice(1);
+                    }else if(v.startsWith('&')){
                         v = v.slice(1);
                     }
                     list.push(v);
-                } else if (v.startsWith('&')) {
-                    v = v.slice(1);
-                    list.push(v);
                 } else if (v.includes("#")) {
+                    v = v.slice(2, -1);
                     list.push(v);
                 } else {
                     return undefined;
@@ -53,8 +53,8 @@ module.exports = (value) => {
             }
         })
         return list;
-    } else {
-        return undefined;
+    }else {
+        return undefined
     }
-
+    
 }
