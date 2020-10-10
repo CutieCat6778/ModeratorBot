89330b5515a7e76a53312ac6df2ffcf8f3ccd16b
NguyenXD06 <thinhnguyenhuuhung@gmail.com>
2020-10-10 22:27:11 +0200

    fixed permissionMissMe"

--- functions/permissionMissMe.js
+++ functions/permissionMissMe.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = function missing(String) {
     const embed = new MessageEmbed()
-    .setColor("#eec4c6")
-    .addField("I don't have this permission", `\`\`\`diff\n- ${String}\`\`\``)
+        .setColor("#eec4c6")
+        .addField("I don't have this permission", `\`\`\`diff\n- ${String}\`\`\``)
         .setTimestamp()
     return embed;
 }
