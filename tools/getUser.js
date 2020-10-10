86f5c94c968c438a9341f6bc6e54d12d1e2e81e9
NguyenXD06 <thinhnguyenhuuhung@gmail.com>
2020-10-10 22:48:11 +0200

    fixed getUser

--- tools/getUser.js
+++ tools/getUser.js
@@ -3,5 +3,5 @@ const Users = require('../models/users');
 module.exports = async(username) => {
     const user = await Users.findOne({username: username.toString()});
     if(!user) return undefined;
-    else if(user) return JSON.parse(user);
+    else if(user) return user;
 }
