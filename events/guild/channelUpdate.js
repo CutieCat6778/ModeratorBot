3394ea4a67cab9f2ad3650a9388cb2bedeacdea0
NguyenXD06 <thinhnguyenhuuhung@gmail.com>
2020-10-10 22:21:32 +0200

    fixed logger

--- events/guild/channelUpdate.js
+++ events/guild/channelUpdate.js
@@ -1,10 +1,11 @@
 module.exports = async (client, oldChannel, newChannel) => {
     if(oldChannel.type == "dm" || newChannel.type == "dm") return;
     const guild = await require('../../tools/getGuild')(client, newChannel.guild.id);
+    console.log(newChannel.id);
     let chanel = guild.channels.find(c => c.id == newChannel.id);
+    console.log(chanel);
     if(!chanel) return;
-    if (chanel && chanel.name.length > 5) return;
-    else if (!chanel) {
+    else if (chanel) {
         chanel.name = newChannel.name;
         chanel.id = newChannel.id;
     }else return;
