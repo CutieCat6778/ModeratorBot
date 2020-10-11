ba17e2c5a9c98e4036778bce6988268c9e8cfa1a
NguyenXD06 <thinhnguyenhuuhung@gmail.com>
2020-10-11 20:38:23 +0200

    fixed inex.js

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ client.start = new Date();
 ["aliases", "commands"].forEach(x => client[x] = new Collection());
 ["afk", "spam", "snipe", "guild"].forEach(x => client[x] = new Map());
 readdirSync("./handlers/").forEach(x => require(`./handlers/${x}`)(client));
-require('./dashboard/server')(client);
 
 client.login(process.env.token);
