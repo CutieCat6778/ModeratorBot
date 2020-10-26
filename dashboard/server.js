a39bc918e9fd3974afd07babfe83be6bb8ff55dc
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-10-26 14:42:30 +0100

    added dashboard

---
+++ dashboard/server.js
@@ -0,0 +1,16 @@
+module.exports = (client) => {
+    const express = require('express');
+    const app = new express();
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.get('/guild', (req, res) => {
+        const body = req.body;
+        const server = client.guild.get(body.serverId);
+        if(!server.moderators.includes(body.userId)) return res.json({status: 69});
+        else if(server.moderators.includes(body.userId)){
+            return res.json({data: server, status: 204});
+        }
+    })
+    app.listen(process.env.port);
+}
