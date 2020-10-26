9df807c79962ab211953f329479d587c7a0d2c09
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-10-26 14:48:10 +0100

    fixed server

--- dashboard/server.js
+++ dashboard/server.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
     app.use(bodyParser.json());
     app.get('/guild', (req, res) => {
         const body = req.body;
-        const server = client.guild.get(body.serverId);
+        const server = client.guild.map(g => g.moderators.includes(body.id));
         if(!server.moderators.includes(body.userId)) return res.json({status: 69});
         else if(server.moderators.includes(body.userId)){
             return res.json({data: server, status: 204});
