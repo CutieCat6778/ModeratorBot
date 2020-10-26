bea2cfbeaa8f2c2774cfa9c56ee121e7d7701869
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-10-26 19:19:51 +0100

    fixed leadboard

--- commands/leveling-system/leaderboard.js
+++ commands/leveling-system/leaderboard.js
@@ -15,8 +15,8 @@ module.exports = {
             .setFooter(`Requested by ${message.member.displayName}`, message.author.displayAvatarURL())
         let leaderboard = await guildCache.leveling.users.sort(await require("../../tools/sortBy")('exp', true, parseInt));
         if(leaderboard.length <= 0) return message.channel.send("There are no infomations has been saved");
-        leaderboard = leaderboard.slice(0, 9);
         leaderboard = await guildCache.leveling.users.sort(await require("../../tools/sortBy")('level', true, parseInt));
+        leaderboard = leaderboard.slice(0, 10);
         for(let i = 0;i < leaderboard.length; i++){
             const user = message.guild.members.cache.get(leaderboard[i].id);
             embed.addField(`[${i + 1}] ${user.displayName}`, `**Exp:** ${leaderboard[i].exp}  **Level:** ${leaderboard[i].level}`)
