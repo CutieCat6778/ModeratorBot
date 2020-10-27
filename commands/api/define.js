2e2629e0dbffd2cd80792307a9fd4a8cf80ba916
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-10-27 21:02:06 +0100

    fixed define

--- commands/api/define.js
+++ commands/api/define.js
@@ -36,7 +36,7 @@ module.exports = {
                     let embed = new MessageEmbed()
                         .setColor("#669fd2")
                         .setTitle(`${body[0].word} - ${body[0].phonetics[0].text}`)
-                        .setDescription(`${body[0].meanings[0].partOfSpeech}\n\n**Definition**\n${body[0].meanings[0].definitions[0].definition}\n\n**Synonyms**\n${body[0].meanings[0].definitions[0].synonyms.join(", ")}\n\n**Example**\n${body[0].meanings[0].definitions[0].example ? body[0].meanings[0].definitions[0].example : "none"}`)
+                        .setDescription(`${body[0].meanings[0].partOfSpeech}\n\n**Definition**\n${body[0].meanings[0].definitions[0].definition}\n\n**Synonyms**\n${body[0].meanings[0].definitions[0].synonyms ? body[0].meanings[0].definitions[0].synonyms.join(", ") : "None"}\n\n**Example**\n${body[0].meanings[0].definitions[0].example ? body[0].meanings[0].definitions[0].example : "none"}`)
                     return message.channel.send(embed);
                 })
             }
