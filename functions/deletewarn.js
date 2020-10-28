a234fe09e8298cd4570113c0480dd267fee49e34
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-10-28 13:43:32 +0100

    fixed delwarn

--- functions/deletewarn.js
+++ functions/deletewarn.js
@@ -1,19 +1,23 @@
-const {MessageEmbed} =require("discord.js");
-
 module.exports = async function warn(message, target, reason, client) {
-    const guild = await require("../tools/getGuild")(client, message.guild.id);
-    let targetData = guild.warn.find(t => t.userId == target.id);
-    if (!targetData) {
-        const object = {
-            userId: target.id, time: 0, reason: " "
+    try {
+        const guild = await require("../tools/getGuild")(client, message.guild.id);
+        const targetData = guild.warn.find(t => t.userId == target.id);
+        if (!targetData) {
+            const object = {
+                userId: target.id, time: 0, reason: " "
+            }
+            guild.warn.push(object);
+            await guild.save();
+            return message.channel.send("That user has no warns");
         }
-        guild.warn.push(object);
-        await guild.save();
-        return message.channel.send(`${target.displayName} doesn't have any warns`);
+        if (targetData.time == 0) return message.channel.send("That user don't have any warns to delete");
+        targetData.time--;
+        targetData.reason = `Deleted one warn for reason __${reason}__`;
+        console.log(guild.warn);
+        await guild.updateOne({warn: guild.warn});
+        console.log(guild.warn)
+        return message.channel.send(`One warn has been removed from **${target.user.username}**`)
+    } catch (e) {
+        return console.log(e);
     }
-    if (targetData.time == 0) return message.channel.send("You don't have any warns to delete");
-    targetData.time--;
-    targetData.reason = `Deleted one warn for reason __${reason}__`;
-    await guild.save();
-    return message.channel.send(`One warn has been removed from **${target.user.username}**`)
 }
