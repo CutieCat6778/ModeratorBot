982bb77ff855716b8a366e9c1c5251162359214b
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-10-28 12:28:21 +0100

    fixed noargs

--- noArgs/moderation/mute.js
+++ noArgs/moderation/mute.js
@@ -4,12 +4,12 @@ module.exports = function help(prefix) {
         .setColor("#669fd2")
         .setTitle("Mute")
         .setDescription(`The mute command's aliases are : \`mute\`, \`m\` or \`mutee\`\n
-            **Temporaly mute**: \`${prefix} mute temp <time> <@user> <reason>\`
+            **Temporaly mute**: \`${prefix} mute <time> <@user> <reason>\`
                 Mute a member for a number of time so after that he will get automaticly unmute
             **Pernamently mute**: \`${prefix} mute <@user> <reason>\`
                 Mute a member so after that he can't talk anymore, until someone unmute him
             **Example**: 
-            \`${prefix} mute temp 10m @steve spam alots\`
+            \`${prefix} mute 10m @steve spam alots\`
             \`${prefix} mute @steve say bad words\`
         `)
     return embed;
