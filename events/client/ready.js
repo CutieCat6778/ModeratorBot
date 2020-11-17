b699ee5e5b3f9119164cf2b886aa106695648f5c
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-11-17 15:15:55 +0100

    fixed brb

--- events/client/ready.js
+++ events/client/ready.js
@@ -11,7 +11,7 @@ module.exports = async (client) => {
             useUnifiedTopology: true
         })
         await require("../../functions/guildCache")(client);
-        const dbl = new DBL("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc2NDkwMTAxNjY5MjU4ODU1NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA1NjIxNTcxfQ.E2Pm7AP-PXVYFKWJiuxfEVq8PlEYGTopW6emV1q6IY8", 
+        const dbl = new DBL(process.env.dbl, 
         { webhookPort: 5002, webhookAuth: '23072006' });
         dbl.on('posted', () => {
             hook.send("DBL posted");
@@ -23,10 +23,10 @@ module.exports = async (client) => {
             console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
         });
         dbl.webhook.on('vote', async vote => {
-            const user = client.users.cache.fetch(vote.user);
+            const user = client.users.fetch(vote.user);
             if(user){
                 hook.send(`${user.username} just voted ${client.user.username}`);
-            }
+            }else hook.send(`Some one with ID ${vote.user} voted ${client.user.username}`)
         });
         await dbl.postStats(client.guilds.cache.size);
         if (!process.env.hook) {
