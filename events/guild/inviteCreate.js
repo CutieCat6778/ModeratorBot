de49a699c59c3dd43ce7cc45d8dc2af85fd58df4
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-11-20 15:35:10 +0100

    fixed

--- events/guild/inviteCreate.js
+++ events/guild/inviteCreate.js
@@ -6,13 +6,14 @@ module.exports = async (client, invite) => {
             if (isNaN(guildCache.logs.id == true)) return;
             const { WebhookClient, MessageEmbed } = require('discord.js');
             const hook = new WebhookClient(guildCache.logs.id, guildCache.logs.token)
+            const user = await invite.guild.members.cache.get(invite.inviter.id);
             const embed = new MessageEmbed()
                 .setColor("#40598F")
                 .setTitle("Logger - Invite created")
                 .addField("Invite channel", invite.channel.name, true)
                 .addField("Invite code", invite.code, true)
                 .addField("URL", invite.url, true)
-                .setDescription(`This invite has been created by **${invite.inviter.displayName}**`)
+                .setDescription(`This invite has been created by **${user.displayName ? user.displayName : user.username}**`)
                 .setTimestamp(new Date())
                 .setFooter(client.user.username, client.user.displayAvatarURL())
             if (hook) {
