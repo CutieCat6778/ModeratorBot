26d77cd7025d082175a6b9ff32c9fcbf8ee21715
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-12-30 16:38:45 +0100

    fixed the inviteDeleted

--- events/guild/inviteDelete.js
+++ events/guild/inviteDelete.js
@@ -6,16 +6,16 @@ module.exports = async (client, invite) => {
             if (isNaN(guildCache.logs.id == true)) return;
             const { WebhookClient, MessageEmbed } = require('discord.js');
             const hook = new WebhookClient(guildCache.logs.id, guildCache.logs.token)
-            const user = await invite.guild.members.cache.get(invite.inviter.id);
+            const user = invite.inviter ? await invite.guild.members.cache.get(invite.inviter.id) : null;
             const embed = new MessageEmbed()
                 .setColor("#40598F")
                 .setTitle("Logger - Invite deleted")
                 .addField("Invite channel", invite.channel.name, true)
                 .addField("Invite code", invite.code, true)
                 .addField("URL", `[click here](${invite.url})`, true)
-                .setDescription(`This invite has been created by **${user.displayName ? user.displayName : user.username}**`)
                 .setTimestamp(new Date())
                 .setFooter(client.user.username, client.user.displayAvatarURL())
+            user ? embed.setDescription(`This invite has been created by **${user.displayName ? user.displayName : user.username}**`) : null;
             if (hook) {
                 return hook.send(embed);
             }
