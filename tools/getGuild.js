2bab99256fd6fde6bba99974f90fa918708242e5
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-01-01 20:38:00 +0100

    fixed guild

--- tools/getGuild.js
+++ tools/getGuild.js
@@ -5,40 +5,5 @@ module.exports = async function getGuild(client, id) {
         const guild = await require('./newGuild')(client, id);
         return guild
     }
-    if (!guild.prefix) {
-        guild.prefix = "mod";
-        await guild.save();
-        getGuild(id);
-    }
-    if (!guild.case) {
-        guild.case = [];
-        await guild.save();
-        getGuild(id);
-    } if (!guild.logs) {
-        guild.logs = {
-            id: " ", enable: false, token: " "
-        }
-        await guild.save();
-        getGuild(id);
-    } if (!guild.capcha) {
-        guild.capcha = {
-            "channels": [], "enable": false
-        };
-        await guild.save();
-        getGuild(id);
-    } if (!guild.textfilter) {
-        guild.textfilter = {
-            "enable": false,
-            "badwords": {
-                "whitelist": [], "blacklist": [], "enable": false
-            }, "links": false, "cap": false
-        };
-        await guild.save();
-        getGuild(id);
-    } if (!guild.rules) {
-        guild.rules = { "enable": false, "channelId": " ", "messageId": " ", "rulesArr": [] };
-        await guild.save();
-        getGuild(id);
-    }
     return guild;
 }
