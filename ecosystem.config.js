d6321d2bb46a9dead1dc96de0d4e6e98e0d39892
Cutie Cat <51492960+CutieCat6778@users.noreply.github.com>
2021-01-10 11:41:38 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     script: 'index.js',
-    name: 'moddy',
+    name: 'moddy-djs',
     watch: '.'
   }],
   deploy : {
@@ -9,7 +9,7 @@ module.exports = {
       user : 'CutieCat6778',
       host : 'Moddy',
       ref  : 'origin/main',
-      repo : 'https://github.com/CutieCat6778/ModeratorBot',
+      repo : 'https://github.com/CutieCat6778/moddy-djs',
       path : '.',
       'pre-deploy-local': 'node index.js',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
