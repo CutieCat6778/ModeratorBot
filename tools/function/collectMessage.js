e36a81acd697c40814aa5bc7ae7a4fb6d5356685
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-02-07 10:52:45 +0100

    fixed collectMessage

--- tools/function/collectMessage.js
+++ tools/function/collectMessage.js
@@ -4,7 +4,7 @@ module.exports = async(message, filter, time) => {
     }
     if(time) obj.time = time;
     const filte = m => m.author.id == message.author.id;
-    let collected = await message.channel.awaitMessages(filte, obj);
+    let collected = await message.channel.awaitMessages(filter ? filter : filte, obj);
     if(!collected) return message.channel.send("Didn't recived any messages");
     if (collected.first().content.toString().toLowerCase() == "cancel") return message.channel.send("Canceled");
     return collected.first() ? collected.first() : collected;
