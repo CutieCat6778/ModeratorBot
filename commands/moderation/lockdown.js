e1b8f6d0e60d67e3d75f16e3a37cc8b75401c8f1
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-02-17 14:00:36 +0100

    added error listener for those 2 new commands

--- commands/moderation/lockdown.js
+++ commands/moderation/lockdown.js
@@ -10,80 +10,84 @@ module.exports = {
         category: "moderation"
     },
     async execute(client, message, args, guildCache) {
-        let data = await require('../../tools/database/getLockdown')(message.channel.id);
-        if(data) {
-            if(data.lock == true){
-                return message.channel.send('This channel has been locked');
+        try {
+            let data = await require('../../tools/database/getLockdown')(message.channel.id);
+            if (data) {
+                if (data.lock == true) {
+                    return message.channel.send('This channel has been locked');
+                }
             }
-        }
-        const embed = new MessageEmbed()
-            .setColor("#40598F")
-            .setDescription('<a:loading:811171036745695283> **Please wait . . . **')
-        const msg = await message.channel.send(embed);
-        const overw = message.channel.permissionOverwrites;
-        const obj = {
-            _id: message.channel.id,
-            lock: true,
-            arr: []
-        }
-        overw.forEach(a => {
-            obj.arr.push({id: a.id, type: a.type, deny: a.deny, allow: a.allow});
-        })
-        const cantUn = [];
-        const guildRoles = message.guild.roles.cache.map(a => a.id);
-        overw.forEach(o => {
-            if (o.type == 'role') {
-                const role = message.guild.roles.cache.get(o.id);
-                if (message.guild.me.roles.highest.position > role.position || role.permission.has(['ADMINISTRATOR']) || role.permission.has('MANAGE_GUILD')) {
-                    const allow = Array.isArray(o.allow) ? o.allow : o.allow.toArray();
-                    const deny = Array.isArray(o.deny) ? o.deny : o.deny.toArray();
-                    const target = ['SEND_MESSAGES', 'SEND_TTS_MESSAGES', 'ADD_REACTIONS', 'EMBED_LINKS', 'ATTACH_FILES'];
-                    target.some(a => {
-                        if (allow.includes(a)) {
-                            allow.splice(a);
+            const embed = new MessageEmbed()
+                .setColor("#40598F")
+                .setDescription('<a:loading:811171036745695283> **Please wait . . . **')
+            const msg = await message.channel.send(embed);
+            const overw = message.channel.permissionOverwrites;
+            const obj = {
+                _id: message.channel.id,
+                lock: true,
+                arr: []
+            }
+            overw.forEach(a => {
+                obj.arr.push({ id: a.id, type: a.type, deny: a.deny, allow: a.allow });
+            })
+            const cantUn = [];
+            const guildRoles = message.guild.roles.cache.map(a => a.id);
+            overw.forEach(o => {
+                if (o.type == 'role') {
+                    const role = message.guild.roles.cache.get(o.id);
+                    if (message.guild.me.roles.highest.position > role.position || role.permission.has(['ADMINISTRATOR']) || role.permission.has('MANAGE_GUILD')) {
+                        const allow = Array.isArray(o.allow) ? o.allow : o.allow.toArray();
+                        const deny = Array.isArray(o.deny) ? o.deny : o.deny.toArray();
+                        const target = ['SEND_MESSAGES', 'SEND_TTS_MESSAGES', 'ADD_REACTIONS', 'EMBED_LINKS', 'ATTACH_FILES'];
+                        target.some(a => {
+                            if (allow.includes(a)) {
+                                allow.splice(a);
+                            }
+                            if (!deny.includes(a)) {
+                                deny.push(a);
+                            }
+                        })
+                        if (guildRoles.includes(o.id)) {
+                            guildRoles.splice(guildRoles.indexOf(o.id), 1);
                         }
-                        if (!deny.includes(a)) {
-                            deny.push(a);
-                        }
-                    })
-                    if (guildRoles.includes(o.id)) {
-                        guildRoles.splice(guildRoles.indexOf(o.id), 1);
+                        o.allow = new Permissions(allow);
+                        o.deny = new Permissions(deny);
+                    } else {
+                        cantUn.push(role.id);
                     }
-                    o.allow = new Permissions(allow);
-                    o.deny = new Permissions(deny);
-                } else {
-                    cantUn.push(role.id);
+                }
+            })
+            if (guildRoles.length != 0) {
+                for (let a of guildRoles) {
+                    obj.arr.push({
+                        id: a,
+                        deny: 0,
+                        allow: 0,
+                        type: "role"
+                    })
+                    const obj1 = {
+                        deny: new Permissions(['SEND_MESSAGES', 'SEND_TTS_MESSAGES', 'ADD_REACTIONS', 'EMBED_LINKS', 'ATTACH_FILES']),
+                        allow: 0,
+                        type: "role"
+                    }
+                    obj1.id = a;
+                    overw.set(a, obj1);
                 }
             }
-        })
-        if (guildRoles.length != 0) {
-            for(let a of guildRoles){
-                obj.arr.push({
-                    id: a,
-                    deny: 0,
-                    allow: 0,
-                    type: "role"
-                })
-                const obj1 = {
-                    deny: new Permissions(['SEND_MESSAGES', 'SEND_TTS_MESSAGES', 'ADD_REACTIONS', 'EMBED_LINKS', 'ATTACH_FILES']),
-                    allow: 0,
-                    type: "role"
-                }
-                obj1.id = a;
-                overw.set(a, obj1);
-            }
+            data = await require("../../tools/database/newLockdown")(obj);
+            await message.channel.overwritePermissions(overw);
+            const embed1 = new MessageEmbed()
+                .setColor('#40598F')
+                .setTitle('Locked this channel')
+                .setDescription(`This channel has been locked, if you want to unlock this channel then you can see more information about \`unlock\` command. Get help about that command, please type anywhere you want \`${guildCache.prefix} help unlock\`!`)
+                .setTimestamp()
+                .setFooter(`Locked by ${message.guild.me.displayName}`, message.guild.iconURL())
+            return client.setTimeout(() => {
+                msg.delete();
+                return send(message, embed1, false);
+            }, 5000);
+        } catch (e) {
+            return require('../../tools/function/error')(e, message);
         }
-        data = await require("../../tools/database/newLockdown")(obj);
-        await message.channel.overwritePermissions(overw);
-        const embed1 = new MessageEmbed()
-            .setColor('#40598F')
-            .setTitle('Locked this channel')
-            .setDescription(`This channel has been locked, if you want to unlock this channel then you can see more information about \`unlock\` command. Get help about that command, please type anywhere you want \`${guildCache.prefix} help unlock\`!`)
-            .setTimestamp()
-            .setFooter(`Locked by ${message.guild.me.displayName}`, message.guild.iconURL())
-        return client.setTimeout(() => {
-            msg.delete();
-            return send(message, embed1, false);
-        }, 5000);
     }
 }
--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -10,37 +10,38 @@ module.exports = {
         category: "moderation"
     },
     async execute(client, message, args, guildCache) {
-        const data = await require('../../tools/database/getLockdown')(message.channel.id);
-        if(!data || !data.lock) return message.channel.send('This channel is not locked');
-        const embed = new MessageEmbed()
-            .setColor("#40598F")
-            .setDescription('<a:loading:811171036745695283> **Please wait . . . **')
-        const msg = await message.channel.send(embed);
-        const overw = message.channel.permissionOverwrites;
-        data.arr.forEach(a => {
-            console.log(a);
-            const role = message.guild.roles.cache.get(a.id);
-            if(message.guild.me.roles.highest.position > role.position){
-                let r = overw.find(b => b.id == a.id);
-                if(r){
-                    console.log(r);
-                    r.deny = r.deny == 55360 ? 0 : a.deny;
-                    r.allow = a.allow;
+        try {
+            const data = await require('../../tools/database/getLockdown')(message.channel.id);
+            if (!data || !data.lock) return message.channel.send('This channel is not locked');
+            const embed = new MessageEmbed()
+                .setColor("#40598F")
+                .setDescription('<a:loading:811171036745695283> **Please wait . . . **')
+            const msg = await message.channel.send(embed);
+            const overw = message.channel.permissionOverwrites;
+            data.arr.forEach(a => {
+                const role = message.guild.roles.cache.get(a.id);
+                if (message.guild.me.roles.highest.position > role.position) {
+                    let r = overw.find(b => b.id == a.id);
+                    if (r) {
+                        r.deny = r.deny == 55360 ? 0 : a.deny;
+                        r.allow = a.allow;
+                    }
                 }
-            }
-        })
-        console.log(overw);
-        await require('../../tools/database/removeLockdown')(message.channel.id)
-        await message.channel.overwritePermissions(overw);
-        const embed1 = new MessageEmbed()
-            .setColor('#40598F')
-            .setTitle('Unlocked this channel')
-            .setDescription(`This channel has been unlocked, if you want to lock this channel again then you can see more information about \`lock\` command. Get help about that command, please type anywhere you want \`${guildCache.prefix} help lock\`!`)
-            .setTimestamp()
-            .setFooter(`Unlocked by ${message.guild.me.displayName}`, message.guild.iconURL())
-        return client.setTimeout(() => {
-            msg.delete();
-            return send(message, embed1, false);
-        }, 5000);
+            })
+            await require('../../tools/database/removeLockdown')(message.channel.id)
+            await message.channel.overwritePermissions(overw);
+            const embed1 = new MessageEmbed()
+                .setColor('#40598F')
+                .setTitle('Unlocked this channel')
+                .setDescription(`This channel has been unlocked, if you want to lock this channel again then you can see more information about \`lock\` command. Get help about that command, please type anywhere you want \`${guildCache.prefix} help lock\`!`)
+                .setTimestamp()
+                .setFooter(`Unlocked by ${message.guild.me.displayName}`, message.guild.iconURL())
+            return client.setTimeout(() => {
+                msg.delete();
+                return send(message, embed1, false);
+            }, 5000);
+        } catch (e) {
+            return require('../../tools/function/error')(e, message);
+        }
     }
 }
