cd7cfcddda60861e2277177089843570ff453b2a
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-02-27 14:12:22 +0100

    fixed modlogs

--- commands/moderation/modlogs.js
+++ commands/moderation/modlogs.js
@@ -18,7 +18,7 @@ module.exports = {
                     .setTitle('Mod logs')
                     .setFooter('All informations has been saved by ' + client.user.username, message.guild.me.user.displayAvatarURL())
                     .setThumbnail(message.guild.iconURL())
-                logs.slice(0, 10).map(a => embed.addField(a.name.slice(0, 1).toUpperCase() + a.name.slice(1), `By: **${message.guild.members.cache.get(a.author)?.user?.tag}** To:  **${message.guild.members.cache.get(a.target)?.user?.tag}**\nReason: **${a?.reason}**`))
+                logs.slice(-6, -1).map(a => embed.addField(a.name.slice(0, 1).toUpperCase() + a.name.slice(1) + ` [${a.num + 1}]`, `By: **${message.guild.members.cache.get(a.author)?.user?.tag}** To:  **${message.guild.members.cache.get(a.target)?.user?.tag}**\nReason: **${a?.reason}**`))
                 return require('../../tools/function/sendMessage')(message, embed, false);
             }
         } catch (e) {
