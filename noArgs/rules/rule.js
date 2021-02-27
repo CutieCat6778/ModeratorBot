7ce52a3c81a353a1c56e42f2cbe783500602015a
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-02-27 15:15:23 +0100

    fixed noargs

--- noArgs/rules/rule.js
+++ noArgs/rules/rule.js
@@ -3,6 +3,6 @@ module.exports = function help(prefix) {
     let embed = new MessageEmbed()
         .setColor("#40598F")
         .setTitle("Rule")
-        .setDescription(`The rule command's aliases are : \`rule\` or \`rules\`\n\n **Setup rule**: \`${prefix} rule [setup]\`\nSetup the rules function\n **Get rule**: \`${prefix} rule [rule_number]\`\nGet rule's content by a number\n **Display rules**: \`${prefix} rule [display]\`\nDisplay all rules\n**Example**: \`\`\`\n${prefix} rule 1\n${prefix} rule setup\`\n\`\`\``)
+        .setDescription(`The rule command's aliases are : \`rule\` or \`rules\`\n\n **Setup rule**: \`${prefix} rule [setup]\`\nSetup the rules function\n **Get rule**: \`${prefix} rule [rule_number]\`\nGet rule's content by a number\n **Display rules**: \`${prefix} rule [display]\`\nDisplay all rules\n**Example**: \`\`\`\n${prefix} rule 1\n${prefix} rule setup\n\`\`\``)
     return embed;
 }
