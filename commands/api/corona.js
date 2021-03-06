ccee55ef0e0c46cb6c091082fc2da4dc62d75a1a
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-03-06 15:19:59 +0100

    fixec corona

--- commands/api/corona.js
+++ commands/api/corona.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     async execute(client, message, args, guildCache) {
         try{
-            let url = "https://api.covid19api.com/summary";
+            let url = `https://www.trackcorona.live/api/countries/${args[0].toUpperCase()}`;
             url = encodeURI(url);
             request({
                 method: 'GET',
@@ -20,30 +20,19 @@ module.exports = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
                 }
             }, function (err, response, body) {
-                if (err) {
+                if (err || JSON.parse(body).code != 200) {
                     return require('../../tools/function/error')(err, message)
                 }
                 body = JSON.parse(body);
-                let country;
-                if(args[0]){
-                    country = body.Countries.find(c => c.CountryCode == args[0].toUpperCase()) || body.Countries.find(c => c.Country.toLowerCase() == args.slice(0).join(" ").toLowerCase());
-                    if (!country) return message.channel.send("Country code/name not found");
-                }else if(!args[0]){
-                    country = body.Global;
-                    country.Country = "Global";
-                    country.Date = body.Date;
-                }
+                const country = body.data[0];
                 let embed = new MessageEmbed()
                     .setColor("#40598F")
-                    .setTitle(`<:covid:774311088334045184> ${country.Country} corona stats`)
-                    .setTimestamp(country.Date)
+                    .setTitle(`<:covid:774311088334045184> ${country.location} corona stats`)
+                    .setTimestamp(country.updated)
                     .setFooter("Last update")
-                    .addField("New confirmed", country.NewConfirmed, true)
-                    .addField("Total confirmed", country.TotalConfirmed, true)
-                    .addField("New deaths", country.NewDeaths, true)
-                    .addField("Total deaths", country.TotalDeaths, true)
-                    .addField("New recovered", country.NewRecovered, true)
-                    .addField("Total recovered", country.TotalRecovered, true)
+                    .addField("Total confirmed", country.confirmed)
+                    .addField("Total deaths", country.dead)
+                    .addField("Total recovered", country.recovered)
                 return require('../../tools/function/sendMessage')(message, embed);
             })
         }catch(e){
